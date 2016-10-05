a64db05ef89fb2585bd54b1344ad266c97b85ffe
Jaime del Palacio <jaimedelpalacio@forio.com>
2016-10-04 22:26:02 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,165 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'));
+
+function xssOff(req, res, next) {
+  res.append('X-XSS-Protection', 0);
+  next();
+}
+
+app.get('/', function (req, res) {
+
+  var html = '\
+  <html>\
+  <body>\
+  <form action="hello" method="get">\
+    What is your name:<br>\
+    <textarea cols="40" name="name"></textarea><br>\
+    <button type="submit">Send</button>\
+  </form>\
+  </body>\
+  </html>';
+
+  res.send(html);
+});
+
+app.get('/hello', function (req, res) {
+  var name = req.query.name;
+
+  res.send('<html><body><h3>Hello ' + name + ' </h3></body></html>');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------------------------------------------
+
+
+app.get('/b64', function (req, res) {
+
+  var html = '\
+  <html>\
+  <body>\
+  <form action="b64" method="post">\
+    base64:<br>\
+    <textarea cols="40" name="name"></textarea><br>\
+    <button type="submit">Send</button>\
+  </form>\
+  </body>\
+  </html>';
+
+  res.send(html);
+});
+
+app.post('/b64', function (req, res) {
+  var name = req.body.name;
+  var n = new Buffer(name, 'base64').toString()
+
+  res.send('<html><body><h3>Result:</h3>' + n + '</body></html>');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------------------------------------------
+
+
+
+
+var todos = [];
+
+
+app.get('/todo', function (req, res) {
+  res.send(todos);
+});
+
+app.post('/todo', function (req, res) {
+  todos.push(req.body.todo);
+  res.send(todos);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
